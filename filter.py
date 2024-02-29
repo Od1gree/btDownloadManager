e87a83f1dd9ce7f95e3dbd4e875758dba951f0d5
Bret Hess <bret.hess@gmail.com>
2024-02-29 13:01:32 -0700

    A single time interval of -t. Added option -x for unconditional filtering.
    This is faster per interval by 10 seconds for 270 torrents.
    -t replaces -a -b.
    -x filters bad clients regardless of leech status.

--- filter.py
+++ filter.py
@@ -53,15 +53,17 @@ def _post_url(url, content):
 
     request.urlopen(request.Request(url, str.encode(content)))
 
-
 class ClientFilter:
 
-    def __init__(self, url='localhost', port=8080, file=None, https=False):
-        self.torrents_dict = {}
+    def __init__(self, url='localhost', port=8080, file=None, https=False, unconditional=False):
+        self.torrents_to_check = {}
         if https:
             self.url_port = "https://" + url + ":" + str(port)
         else:
             self.url_port = "http://" + url + ":" + str(port)
+        self.unconditional = False
+        if unconditional:
+            self.unconditional = True
         self.string_list = []
         self.config_json = None
         if file:
@@ -78,14 +80,18 @@ class ClientFilter:
 
         print('connecting to server ' + self.url_port)
 
-    def get_torrent_list(self):
+    def get_torrents_to_check(self):
         server_url = self.url_port + "/api/v2/sync/maindata"
         torrents_str = _get_url(server_url)
+        self.torrents_to_check = {}
         obj = json.loads(torrents_str)
-        self.torrents_dict = obj['torrents']
+        torrents = obj['torrents']
+        for torrent_hash in torrents.keys():
+            if torrents[torrent_hash]['num_leechs'] > 0 or self.unconditional:
+                self.torrents_to_check[torrent_hash] = torrents[torrent_hash]
+        pass
 
     def clear_banned_ip_list(self):
-        '''Code by Sam'''
         self.config_json = json.loads(_get_url(self.url_port + "/api/v2/app/preferences"))
         banned_ip_str = ''
         self.config_json['banned_IPs'] = banned_ip_str
@@ -96,13 +102,13 @@ class ClientFilter:
         """
         Get all the connected peers using torrent hash list and ban the matched peer.
         """
-        # the banned_ip value is ip address string split with '\n'
         self.config_json = json.loads(_get_url(self.url_port + "/api/v2/app/preferences"))
         banned_ip_str = self.config_json["banned_IPs"]
-
-        for item in self.torrents_dict:
-            if self.torrents_dict[item]['num_leechs'] > 0:
-                peers = json.loads(self._get_peers_list(item))['peers']
+        
+        active_torrents = json.loads(_get_url(self.url_port + "/api/v2/torrents/info?filter=active"))
+        for torrent in active_torrents:
+            if torrent['hash'] in self.torrents_to_check:
+                peers = json.loads(self._get_peers_list(torrent['hash']))['peers']
                 for ip_port in peers:
                     for xl in self.string_list:
                         if xl in peers[ip_port]['client']:
@@ -112,14 +118,14 @@ class ClientFilter:
                             print(str.encode(time_str + 'banned ' + peers[ip_port]['ip']
                                   + ' client name:' + peers[ip_port]['client']))
 
-        self.config_json['banned_IPs'] = banned_ip_str
-        _post_url(self.url_port + "/api/v2/app/setPreferences", 'json=' + json.dumps(self.config_json))
+            self.config_json['banned_IPs'] = banned_ip_str
+            _post_url(self.url_port + "/api/v2/app/setPreferences", 'json=' + json.dumps(self.config_json))
 
     def _get_peers_list(self, torrent_hash):
         server_url = self.url_port + "/api/v2/sync/torrentPeers?rid=0&hash=" + torrent_hash
         return _get_url(server_url)
 
-    def start(self, torrent_time_cycle=300, filter_time_cycle=10, clear_hours=None):
+    def start(self, filter_time_cycle=10, clear_hours=None):
         """
         Run a while true loop to ban matched ip with certain time interval.
         :param torrent_time_cycle: Time interval in sec to check the torrent list.
@@ -130,10 +136,7 @@ class ClientFilter:
         start_time = time.perf_counter()
         def t():
             return round(time.perf_counter() - start_time, 1)
-
-        print('torrent time interval is {} sec'.format(torrent_time_cycle))
         print('filter time interval is {} sec'.format(filter_time_cycle))
-
         clear_time_cycle = None
         if clear_hours:
             clear_time_cycle = clear_hours * 3600
@@ -141,42 +144,43 @@ class ClientFilter:
             elif int(clear_hours) > 1: digits = 1
             else: digits = 3
             print('clear time interval is {} hr'.format(round(clear_hours,digits)))
-
-        i_cycle_torrent = 0
+        if self.unconditional:
+            print('-x is set: blocks clients regardless of leeching status')
         i_cycle_clear = 0
         i_cycle_filter = 0
+        t_old = t()
         while True:
-            if t() > i_cycle_torrent * torrent_time_cycle - 1:
-                self.get_torrent_list()
-                i_cycle_torrent += 1
             if clear_time_cycle and t() > (i_cycle_clear + 1) * clear_time_cycle:
                 self.clear_banned_ip_list()
                 i_cycle_clear += 1
+            self.get_torrents_to_check()
             self.filter()
             i_cycle_filter += 1
             t_cycle_remaining = i_cycle_filter * filter_time_cycle - t()
             if t_cycle_remaining > 0:
                 time.sleep(t_cycle_remaining)
-
+            t_new = t()
+            print(t_new-t_old)
+            t_old = t()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Ban Xunlei peers in qBittorrent connections.',
-                                     epilog='eg: python3 filterSamThenBCH.py -u localhost -p 8080 -a 300 -b 10')
+    parser = argparse.ArgumentParser(description='Ban bad peers in qBittorrent connections.',
+                                     epilog='eg: python3 filter.py -u localhost -p 8080 -t 10')
     parser.add_argument('-u', default='localhost',
                         help='url of the service without \'http://\' or \'https://\'')
     parser.add_argument('-p', default=8080, type=int,
                         help='port number. Default=8080')
-    parser.add_argument('-a', default=300,  type=int,
-                        help='time interval to fetch torrents list in seconds. Default=300')
-    parser.add_argument('-b', default=10,   type=int,
-                        help='time interval to fetch peers list in seconds. Default=10')
+    parser.add_argument('-t', default=10,  type=int,
+                        help='time interval between filter checks')
     parser.add_argument('-f', default=None, type=str,
                         help='path to the string-filter file. Each line contains a string. Default=None')
     parser.add_argument('-s', default=False, action="store_true",
                         help='use https protocol. Default=http')
     parser.add_argument('-c', default=None, type=float,
                         help='time interval to clear torrents list in hours (decimal OK). Default=None')
+    parser.add_argument('-x', default=False, action="store_true",
+                        help='blocks clients unconditionally regardless of leeching status')
 
     config = parser.parse_args()
-    f = ClientFilter(url=config.u, port=config.p, file=config.f, https=config.s, )
-    f.start(torrent_time_cycle=config.a, filter_time_cycle=config.b, clear_hours=config.c)
+    f = ClientFilter(url=config.u, port=config.p, file=config.f, https=config.s, unconditional=config.x)
+    f.start(filter_time_cycle=config.t, clear_hours=config.c)
